53570a23b4c04a3ba127436e5047382cae34c36b
Newatia <Archana.Newatia@fda.gov>
2021-10-20 16:56:01 -0400

    updated to ForceUpdateDirtyMakerMixin

--- gsrs-module-applications-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/application/application/models/ApplicationCommanData.java
+++ gsrs-module-applications-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/application/application/models/ApplicationCommanData.java
@@ -5,7 +5,7 @@ import gsrs.security.GsrsSecurityUtils;
 import ix.core.models.Indexable;
 import ix.core.models.Principal;
 import ix.core.models.UserProfile;
-import ix.core.models.ForceUpdateDirtyMixin;
+import gsrs.ForceUpdateDirtyMakerMixin;
 import ix.ginas.models.serialization.GsrsDateDeserializer;
 import ix.ginas.models.serialization.GsrsDateSerializer;
 
@@ -20,7 +20,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @MappedSuperclass
-public class ApplicationCommanData extends AbstractGsrsEntity implements ForceUpdateDirtyMixin{
+public class ApplicationCommanData extends AbstractGsrsEntity implements ForceUpdateDirtyMakerMixin{
 
   //  @Id
   //  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "appSeq")
