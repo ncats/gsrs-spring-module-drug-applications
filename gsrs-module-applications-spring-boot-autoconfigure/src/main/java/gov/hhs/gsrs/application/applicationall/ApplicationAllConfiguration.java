c8bf6101e4a88f11ac04ee14819e94dece081889
Mitch Miller <mitch.miller@thinkscience.us>
2025-12-11 14:53:46 -0500

    updating to version 3.1.4-SNAPSHOT

--- gsrs-module-applications-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/application/applicationall/ApplicationAllConfiguration.java
+++ gsrs-module-applications-spring-boot-autoconfigure/src/main/java/gov/hhs/gsrs/application/applicationall/ApplicationAllConfiguration.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @AutoConfigureAfter(JpaRepositoriesAutoConfiguration.class)
-@Import({ApplicationAllStarterEntityRegistrar.class, PrivilegeService.class})
+@Import({ApplicationAllStarterEntityRegistrar.class})
 public class ApplicationAllConfiguration {
 }
